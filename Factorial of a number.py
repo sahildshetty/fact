a2fdaa7919eea5be0a86ccbbc7b5aeedeaf0ff22
sahildshetty <64770887+sahildshetty@users.noreply.github.com>
2020-09-30 19:54:18 +0530

    Add files via upload
    To calculate factorial of a number using python

---
+++ Factorial of a number.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[47]:
+
+
+
+def factorial(x): #function intialization
+    y=1
+    if int(x)>=1:
+        for i in range(1,x):
+            y=y*i
+    print(y)    
+
+
+# In[48]:
+
+
+factorial(5)     #call factorial function
+
+
+# In[ ]:
+
+
+
+
